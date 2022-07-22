5d22ca6e090a6121dcc662f9796ab8d499c0af0c
Colin Faivre <colin.faivre@gmail.com>
2022-07-22 11:49:27 +0200

    remove packer auto install and unused packages

--- nvim/.config/nvim/lua/colinfaivre/plugins.lua
+++ nvim/.config/nvim/lua/colinfaivre/plugins.lua
@@ -1,20 +1,5 @@
 local fn = vim.fn
 
--- Automatically install packer
-local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-    PACKER_BOOTSTRAP = fn.system {
-        "git",
-        "clone",
-        "--depth",
-        "1",
-        "https://github.com/wbthomason/packer.nvim",
-        install_path,
-    }
-    print "Installing packer close and reopen Neovim..."
-    vim.cmd [[packadd packer.nvim]]
-end
-
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
 vim.cmd [[
     augroup packer_user_config
@@ -73,17 +58,12 @@ return packer.startup(function(use)
     -- Telescope
     use { "nvim-telescope/telescope.nvim" }
 
-    -- Treesitter
-    use {
-        "nvim-treesitter/nvim-treesitter",
-        config = function()
-            require("nvim-treesitter").setup()
-        end
-
-    }
     -- Git
     use("TimUntersberger/neogit")
-    
+    use {
+	    'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' },
+	    config = function() require('gitsigns').setup() end
+	}
     -- DAP
 
     -- Automatically set up your configuration after cloning packer.nvim
